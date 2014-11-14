de9b1d325aba6b072fb3f3463e91ad51df753dc8
Antonio Ramirez <antonio@ramirezcobos.com>
2014-11-14 18:47:06 +0100

    Fixes #8
    Thanks!

--- overlays/Rectangle.php
+++ overlays/Rectangle.php
@@ -4,7 +4,7 @@
  * @link http://2amigos.us
  * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
  */
-namespace dosamigos\google\maps;
+namespace dosamigos\google\maps\overlays;
 
 use dosamigos\google\maps\overlays\RectangleOptions;
 use yii\base\InvalidConfigException;
