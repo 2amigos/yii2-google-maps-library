7411d8722e57c3cefa6df0688114d42e3e349425
Petra Barus <petra.barus@gmail.com>
2015-01-30 16:07:57 +0700

    Add backward compatiblity.

--- MapAsset.php
+++ MapAsset.php
@@ -15,7 +15,8 @@ use yii\web\AssetBundle;
  * Registers the google maps api
  *
  * To update the key or other options like language, version, or library
- * use the assetManager customization
+ * use the assetManager customization.
+ * To get key, please visit https://code.google.com/apis/console/
  *
  *      'components' => [
  *          'assetManager' => [
@@ -48,6 +49,23 @@ class MapAsset extends AssetBundle
      */
     public function init()
     {
+
+	// BACKWARD COMPATIBILITY
+	// To configure please, add `googleMapsApiKey` parameter to your application configuration
+	// file with the value of your API key. To get yours, please visit https://code.google.com/apis/console/.
+	$key = @Yii::$app->params['googleMapsApiKey'];
+	// To configure please, add `googleMapsLibraries` parameter to your application configuration
+	$libraries = @Yii::$app->params['googleMapsLibraries'];
+	// To configure please, add `googleMapsLanguage` parameter to your application configuration
+	$language = @Yii::$app->params['googleMapsLanguage'];
+
+	$this->options = array_merge($this->options, array_filter([
+            'key' => $key,
+            'libraries' => $libraries,
+            'language' => $language
+	]));
+	// BACKWARD COMPATIBILITY
+
         $this->js[] = 'https://maps.googleapis.com/maps/api/js?'. http_build_query($this->options);
     }
 } 
